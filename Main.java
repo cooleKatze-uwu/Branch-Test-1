9350d1359a831853233123ce33a2e1b38c10665b
cooleKatze-uwu <purple_blueberry_jam@protonmail.com>
2026-08-28 01:59:49 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Short calculator application
+        // +,-,*,/ as operators
+
+        while(true) {
+            input();
+            calculate();
+        }
+
+        
+
+    }
+
+    public static void input() {
+
+        };
+
+        public static void calculate(double a, double b, char operator) {
+
+        }
+}
