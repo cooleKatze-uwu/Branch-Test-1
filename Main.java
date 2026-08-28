38897e4a38032b9fea115eed70ca30ce9f70e31f
cooleKatze-uwu <purple_blueberry_jam@protonmail.com>
2026-08-28 02:37:44 +0200

    Implementing switch-case into calculate()

--- Main.java
+++ Main.java
@@ -2,21 +2,45 @@ public class Main {
     public static void main(String[] args) {
         // Short calculator application
         // +,-,*,/ as operators
+        // input() calls calculate()
+    
+       
 
         while(true) {
             input();
-            calculate();
         }
 
         
-
     }
 
     public static void input() {
 
-        };
+    };
 
-        public static void calculate(double a, double b, char operator) {
+    public static void calculate(double a, double b, char operator) {
+        double result;
+        switch (operator) {
+            case '+':
+                result = a + b;
+                break;
 
+            case '-':
+                result = a - b;
+                break;
+
+            case '*':
+                result = a * b;
+                break;
+
+            case '/':
+                result = a / b;
+                break;
+        
+            default:
+                break;
         }
+
+        
+    }
+
 }
